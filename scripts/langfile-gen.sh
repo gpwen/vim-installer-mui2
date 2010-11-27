1587dacba5a763d438f18386ce8ff5e258087644
Guopeng Wen <wenguopeng@gmail.com>
2010-11-27 21:37:05 +0800

    Add support for locale name in scripts

--- scripts/langfile-gen.sh
+++ scripts/langfile-gen.sh
@@ -13,6 +13,9 @@ CMD=$(basename $0)
 # Name of the OS:
 SYSTEM_TYPE=$(uname -s)
 
+# Name of the locale mapping file:
+NSIS_LC_MAPPING=locale_mapping.dat
+
 # Name of the English template file:
 OPT_EN_LANG_FILE=/tmp/english.nsi.$$
 
@@ -28,6 +31,7 @@ LINK_LANG_FILE_BASE="$LINK_GIT_RAW_BASE/master/nsis/lang"
 # Command line options:
 OPT_NSIS_PATH=
 OPT_WIKI_PAGE=
+OPT_DUMP_LANG=
 
 # Remove temporary file when exit:
 trap "rm -rf ${OPT_EN_LANG_FILE}" EXIT
@@ -52,6 +56,7 @@ USAGE:
     $CMD [-s <nsis-path>] [-w]
 
 Where:
+  -d             : Dump name of all language supported by NSIS.
   -s <nsis-path> : NSIS install path, cygwin format.  This script will try to
                    detect it automatically from path of makensis command if
                    not specified.
@@ -65,8 +70,11 @@ return 0
 # Main script begins here                                                 {{{1
 # ============================================================================
 # Parse the command line:
-while getopts :s:hw option; do
+while getopts :s:dhw option; do
     case $option in
+        d) OPT_DUMP_LANG=1
+           ;;
+
         h) print_usage
            exit 0
            ;;
@@ -116,6 +124,12 @@ if [[ ! -d "$OPT_NSIS_PATH" ]]; then
     exit 1
 fi
 
+# Make sure locale mapping file exist under the current directory:
+if [[ ! -r "$NSIS_LC_MAPPING" ]]; then
+    print -u2 "$CMD: Cannot access locale mapping table $NSIS_LC_MAPPING!"
+    exit 1
+fi
+
 # Output wiki-page:
 if [[ $OPT_WIKI_PAGE -ne 0 ]]; then
     print "<TABLE BORDER=\"1\" ALIGN=\"CENTER\" CELLSPACING=\"0\""
@@ -125,6 +139,8 @@ if [[ $OPT_WIKI_PAGE -ne 0 ]]; then
     print "      <TH>Language</TH>"
     print "      <TH>&nbsp;</TH>"
     print "      <TH>Locale ID</TH>"
+    print "      <TH>&nbsp;</TH>"
+    print "      <TH>Locale Name</TH>"
     print "      <TH>&nbsp;</TH>"
     print "      <TH>Template</TH>"
     print "      <TH>&nbsp;</TH>"
@@ -143,6 +159,12 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
     # Name of the language:
     NSIS_LANG_NAME=${LANG_FILE_FULL##*/}
     NSIS_LANG_NAME=${NSIS_LANG_NAME%.*}
+
+    # Only dump all languages supported by NSIS:
+    if [[ $OPT_DUMP_LANG -ne 0 ]]; then
+        print $NSIS_LANG_NAME
+        continue
+    fi
 
     # ID of the language, all upper case:
     typeset -u NSIS_LANG_ID="LANG_$NSIS_LANG_NAME"
@@ -166,6 +188,15 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
                 last;
             }' "$LANG_FILE_FULL")
 
+    # Determine GNU gettext style locale name:
+    NSIS_LC_NAME=$(grep -iE ": ${NSIS_LANG_NAME}$" $NSIS_LC_MAPPING)
+    NSIS_LC_NAME=${NSIS_LC_NAME%%:*}
+    NSIS_LC_NAME=${NSIS_LC_NAME%% *}
+    if [[ -z "$NSIS_LC_NAME" ]]; then
+        print -u2 "$CMD: Cannot find locale name for [$NSIS_LANG_NAME]!"
+        NSIS_LC_NAME="UNKNOWN"
+    fi
+
     # Output wiki-page:
     if [[ $OPT_WIKI_PAGE -ne 0 ]]; then
         LINK_TEMP="$LINK_LANG_TMPL_BASE/$NSIS_LANG_FILE"
@@ -173,6 +204,8 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
         print "      <TD>$NSIS_LANG_NAME</TD>"
         print "      <TD>&nbsp;</TD>"
         print "      <TD>$NSIS_LCID</TD>"
+        print "      <TD>&nbsp;</TD>"
+        print "      <TD>$NSIS_LC_NAME</TD>"
         print "      <TD>&nbsp;</TD>"
         print "      <TD><A HREF=\"$LINK_TEMP\">$NSIS_LANG_FILE</A></TD>"
         print "      <TD>&nbsp;</TD>"
@@ -230,6 +263,7 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
     NSIS_LANG_NAME=$NSIS_LANG_NAME \
     NSIS_LANG_ID=$NSIS_LANG_ID \
     NSIS_LCID=$NSIS_LCID \
+    NSIS_LC_NAME=$NSIS_LC_NAME \
     NSIS_LANG_FILE=$NSIS_LANG_FILE \
     perl -pi'nsis_orig_*.bak' -e '
         s/[\s\r\n]+$//iox;
@@ -238,7 +272,7 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
         if (!$content_start)
         {
             $content_start =
-                /^\!insertmacro \s+ MUI_LANGUAGE \s+ "English"$/xo;
+                /^\${VimAddLanguage} \s+ "English" \s+ "en"$/xo;
 
             $_ = ($content_start) ?
                "# vi:set ts=8 sts=4 sw=4 fdm=marker:\n" .
@@ -247,10 +281,13 @@ for LANG_FILE_FULL in "$OPT_NSIS_PATH"/*.nlf; do
                "language strings for gvim NSIS installer.\n" .
                "#\n" .
                "# Locale ID    : $ENV{NSIS_LCID}\n" .
+               "# Locale Name  : $ENV{NSIS_LC_NAME}\n" .
 	       "# fileencoding :\n" .
                "# Author       :\n" .
                "\n" .
-               "!insertmacro MUI_LANGUAGE \"$ENV{NSIS_LANG_NAME}\"\n" : "";
+               "!include \"helper_util.nsh\"\n" .
+               "\${VimAddLanguage} \"$ENV{NSIS_LANG_NAME}\" " .
+               "\"$ENV{NSIS_LC_NAME}\"\n" : "";
 
             next;
         }
